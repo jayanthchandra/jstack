a1aa232878883b7781dfab0747f9a82cc678befc
jayanthchandra <jayanthchandra14@gmail.com>
2015-06-22 22:33:17 +0530

    added new function and routing

---
+++ admin-openrc.sh
@@ -0,0 +1,5 @@
+export OS_USERNAME=username
+export OS_PASSWORD=password
+export OS_TENANT_NAME=projectName
+export OS_AUTH_URL=https://identityHost:portNumber/v2.0
+
--- server.py
+++ server.py
@@ -4,6 +4,19 @@ app=Flask(__name__,static_url_path="")
 @app.route('/')
 def index():
 	return current_app.send_static_file('index.html')
+	
+@app.route('/nova')
+def nova():
+	return current_app.send_static_file('nova.html')
+
+@app.route('/keystone')
+def keystone():
+	return current_app.send_static_file('keystoe.html')
+
+@app.route('/cinder')
+def cinder():
+	return current_app.send_static_file('cinder.html')
+
 
 if __name__ == '__main__':
     app.run()
