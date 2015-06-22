c5c10972088b765d290f30ee379736897a9c293c
jayanthchandra <jayanthchandra14@gmail.com>
2015-06-22 15:44:03 +0530

    Flask Server Working

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask,current_app
+app=Flask(__name__,static_url_path="")
+
+@app.route('/')
+def index():
+	return current_app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run()
